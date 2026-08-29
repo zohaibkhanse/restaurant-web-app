5018962a8ca891ed161a2394b2c52b23432be71e
ZOHAIB KHAN <zohaibkhanse@gmail.com>
2026-08-29 16:11:21 +0500

    ansibe: ansible workflow added

---
+++ ansible/s.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ansible-playbook -i inventory/development.ini playbooks/common.yml --become --ask-become-pass
+ansible-playbook -i inventory/development.ini playbooks/nginx.yml --become --ask-become-pass
+export RELEASE_ID=$(date +%Y%m%d%H%M%S)
+ansible-playbook -i inventory/development.ini playbooks/deploy.yml  --become --ask-become-pass  -e "artifact_path=/home/zohaib/devops/course-2/restaurant-web-app/dist"   -e "release_id=$RELEASE_ID" 
+
+
+ansible-playbook \
+  -i inventory/development.ini \
+  playbooks/deploy.yml \
+  -e "artifact_path=../dist" \
+  -e "release_id=$RELEASE_ID"
